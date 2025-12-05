17cbfe26147186208eb88806bec63aee6f5c396d
Nickk1917 <niickslasher@gmail.com>
2025-12-05 16:14:35 +0530

    Update Vite config to include allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,22 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: 'https://kronstadt-ai-learn.onrender.com',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+  return {
+    server: {
+      port: 3000,
+      allowedHosts: ['kronstadt-ai-learn.onrender.com'], 
+      host: 'kronstadt-ai-learn.onrender.com',          
+    },
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        
       }
-    };
+    }
+  }
 });
